e4eea585c53506b4740468ae2817abe3ffa468df
Ceo-Nxtawe <ceo@nxtwave.fr>
2024-12-23 16:00:48 +0100

    Update proxy.py

--- Dragon/proxy.py
+++ Dragon/proxy.py
@@ -38,23 +38,25 @@ def get_data_with_proxy(url: str, headers: dict = None, json_payload: dict = Non
     Raises:
         RuntimeError: In case of request failure.
     """
-    try:
-        current_proxy = next(proxy_cycle)
-        
-        session = requests.Session()
-        session.proxies.update(current_proxy)
+    attempts = 0
+    MAX_ATTEMPTS = 3
+    while attempts < MAX_ATTEMPTS:
+        try:
+            current_proxy = next(proxy_cycle)
+            print(f"Using proxy: {current_proxy} for {url}")
+            session = requests.Session()
+            session.proxies.update(current_proxy)
 
-        # Effectuer une requête GET ou POST
-        if json_payload is not None:
-            # POST with JSON payload
-            response = session.post(url, headers=headers, json=json_payload, verify=False, timeout=timeout)
-        else:
-            # GET classique
-            response = session.get(url, headers=headers, verify=False, timeout=timeout)
-        
-        response.raise_for_status()  # Vérifies HTTP errors
-        return response
-    except requests.Timeout:
-        raise RuntimeError(f"Reaching timeout while requesting {url}.")
-    except requests.RequestException as e:
-        raise RuntimeError(f"HTTP error while requesting {url}: {str(e)}")
+            # Effectuer une requête GET ou POST
+            if json_payload is not None:
+                # POST with JSON payload
+                response = session.post(url, headers=headers, json=json_payload, verify=False, timeout=timeout)
+            else:
+                # GET classique
+                response = session.get(url, headers=headers, verify=False, timeout=timeout)
+            
+            response.raise_for_status()  # Vérifies HTTP errors
+            return response
+        except (requests.Timeout, requests.RequestException) as e:
+            attempts += 1
+    raise RuntimeError(f"All attempts failed while requesting {url}.")
